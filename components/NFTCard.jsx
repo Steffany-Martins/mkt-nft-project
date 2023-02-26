855510de69201743e45d530bca9d93418066b6a8
Steffany-Martins <steffany@zee-dog.com>
2023-02-26 19:36:11 -0300

    fix image nftcard

--- components/NFTCard.jsx
+++ components/NFTCard.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useContext } from 'react';
 import { NFTContext } from '../context/NFTContext';
-import images from '../assets';
+// import images from '../assets';
 import { shortenAddress } from '../utils';
 
 const NFTCard = ({ nft, onProfilePage }) => {
@@ -11,7 +11,7 @@ const NFTCard = ({ nft, onProfilePage }) => {
     <Link href={{ pathname: '/nft-details', query: nft }} passHref>
       <div className="flex-1 p-4 m-4 bg-white shadow-md cursor-pointer min-w-215 max-w-max xs:max-w-none sm:w-full sm:min-w-155 minmd:min-w-256 minlg:min-w-327 dark:bg-nft-black-3 rounded-2xl minlg:m-8 sm:my-2 sm:mx-2">
         <div className="relative w-full overflow-hidden h-52 sm:h-36 mind:h-60 minlg:h-300 rounded-2xl">
-          <Image src={nft.image || images[`nft${nft.i}`]} width={200} height={200} alt={`nft${nft.i}`} />
+          <Image src={nft.image} layout="fill" objectFit="cover" alt={`nft${nft.i}`} />
         </div>
         <div className="flex flex-col mt-3">
           <p className="text-sm font-semibold font-poppins dark:text-white text-nft-black-1 minlg:text-xl">{nft.name}</p>
